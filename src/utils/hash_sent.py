1a5df0717b8906248b91baf0c198e7ea085e8745
Tom W <tomw@DEV-LT-4742.local>
2016-06-24 09:03:55 -0700

    Added a few comments

--- src/utils/hash_sent.py
+++ src/utils/hash_sent.py
@@ -26,10 +26,15 @@ import math
 	#Description: Splits the text into the sizes needed for hashing. Returns a list of strings split based on the parameters provided
 
 def hashDocument(text, window_size = 25, words = False, sliding = True, sliding_window = 4):
+	#Error if sliding_window is bigger than or equal to window_size and sliding is turned on
 	if sliding_window >= window_size and sliding:
 		raise ValueError("sliding_window must be smaller than window_size;")
+	#
+	#Split text into distinct blocks for hashing
 	textSplit = splitText(text, window_size, words, sliding, sliding_window)
 	returnText = ""
+	#
+	#Md5 hash each part of the text and return the text as a long string of these hashes
 	for item in textSplit:
 		m = hashlib.md5()
 		m.update(item.encode("utf-8"))
@@ -41,12 +46,16 @@ def splitText(text, window_size, words, sliding, sliding_window):
 	if words:
 		tempSplit = text.split(" ")
 		if sliding:
+			#Grab each window of window_size words from the text sliding sliding_window over each iteration.
 			textSplit = [" ".join(tempSplit[i:i+window_size]) for i in range(0, len(tempSplit), sliding_window) if (i + window_size) < (len(tempSplit) + sliding_window)] 
 		else:
+			#Grab each window of window_size words from the text without overlap
 			textSplit = [" ".join(tempSplit[i:i+window_size]) for i in range(0, len(tempSplit), window_size)] 
 	else:
 		if sliding:
+			#Grab each window of window_size characters from the text sliding sliding_window over each iteration.
 			textSplit = [text[i:i+window_size] for i in range(0, len(text), sliding_window) if (i + window_size) < (len(text) + sliding_window)] 
 		else:
+			#Grab each window of window_size characters from the text without overlap
 			textSplit = [text[i:i+window_size] for i in range(0, len(text), window_size)] 
 	return textSplit
