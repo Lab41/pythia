4c3cfbfa12fe300649456f29a9dfaac8a174d337
Audrey Flower <aflower@dev-lt-4257.local>
2016-06-27 13:24:09 -0700

    codecov

--- src/tests/test_pipeline.py
+++ src/tests/test_pipeline.py
@@ -2,30 +2,30 @@ from src.pipelines import master_pipeline
 import subprocess
 import os
 
+'''
+TEST FROM SRC
+'''
 def test_no_directory():
     print(os.getcwd())
-    os.chdir('../pipelines')
+    os.chdir('pipelines')
     dir_ = os.getcwd()
     file_ = os.path.join(dir_, "master_pipeline.py")
     result = subprocess.run([file_])
     assert result.returncode == 2
 
 def test_no_features():
-    os.chdir('../pipelines')
     dir_ = os.getcwd()
     file_ = os.path.join(dir_, "master_pipeline.py")
     result = subprocess.run([file_, "dir"])
     assert result.returncode == 1
 
 def test_no_algorithms():
-    os.chdir('../pipelines')
     dir_ = os.getcwd()
     file_ = os.path.join(dir_, "master_pipeline.py")
     result = subprocess.run([file_, "dir", "-c"])
     assert result.returncode == 3
 
 def test_successful_run():
-    os.chdir('../pipelines')
     dir_ = os.getcwd()
     file_ = os.path.join(dir_, "master_pipeline.py")
     result = subprocess.run([file_, "dir", "-c", "-s"])
