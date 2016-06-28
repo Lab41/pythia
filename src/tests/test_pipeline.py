cd115651b04419ba8cfcd9e0e28065e2fd0b50d7
Audrey Flower <aflower@dev-lt-4257.local>
2016-06-28 14:52:18 -0700

    restructure

--- src/tests/test_pipeline.py
+++ src/tests/test_pipeline.py
@@ -5,28 +5,29 @@ import os
 '''
 TEST FROM SRC
 '''
-def test_no_directory():
-    print(os.getcwd())
-    os.chdir('pipelines')
-    dir_ = os.getcwd()
-    file_ = os.path.join(dir_, "master_pipeline.py")
-    result = subprocess.run([file_])
-    assert result.returncode == 2
+class test_master_pipeline():
+    def test_no_directory():
+        print(os.getcwd())
+        os.chdir('pipelines')
+        dir_ = os.getcwd()
+        file_ = os.path.join(dir_, "master_pipeline.py")
+        result = subprocess.run([file_])
+        assert result.returncode == 2
 
-def test_no_features():
-    dir_ = os.getcwd()
-    file_ = os.path.join(dir_, "master_pipeline.py")
-    result = subprocess.run([file_, "dir"])
-    assert result.returncode == 1
+    def test_no_features():
+        dir_ = os.getcwd()
+        file_ = os.path.join(dir_, "master_pipeline.py")
+        result = subprocess.run([file_, "dir"])
+        assert result.returncode == 1
 
-def test_no_algorithms():
-    dir_ = os.getcwd()
-    file_ = os.path.join(dir_, "master_pipeline.py")
-    result = subprocess.run([file_, "dir", "-c"])
-    assert result.returncode == 3
+    def test_no_algorithms():
+        dir_ = os.getcwd()
+        file_ = os.path.join(dir_, "master_pipeline.py")
+        result = subprocess.run([file_, "dir", "-c"])
+        assert result.returncode == 3
 
-def test_successful_run():
-    dir_ = os.getcwd()
-    file_ = os.path.join(dir_, "master_pipeline.py")
-    result = subprocess.run([file_, "dir", "-c", "-s"])
-    assert result.returncode == 0
+    def test_successful_run():
+        dir_ = os.getcwd()
+        file_ = os.path.join(dir_, "master_pipeline.py")
+        result = subprocess.run([file_, "dir", "-c", "-s"])
+        assert result.returncode == 0
