68a24893e48e527c7de75d6aefe1f99b39f7f829
Audrey Flower <aflower@dev-lt-4257.local>
2016-06-28 16:13:31 -0700

    restructure

---
+++ src/tests/test_master_pipeline.py
@@ -0,0 +1,33 @@
+import py.test
+from src.pipelines import master_pipeline
+import subprocess
+import os
+
+'''
+TEST FROM SRC
+'''
+def test_no_directory():
+    print(os.getcwd())
+    os.chdir('pipelines')
+    dir_ = os.getcwd()
+    file_ = os.path.join(dir_, "master_pipeline.py")
+    result = subprocess.run([file_])
+    assert result.returncode == 2
+
+def test_no_features():
+    dir_ = os.getcwd()
+    file_ = os.path.join(dir_, "master_pipeline.py")
+    result = subprocess.run([file_, "dir"])
+    assert result.returncode == 1
+
+def test_no_algorithms():
+    dir_ = os.getcwd()
+    file_ = os.path.join(dir_, "master_pipeline.py")
+    result = subprocess.run([file_, "dir", "-c"])
+    assert result.returncode == 3
+
+def test_successful_run():
+    dir_ = os.getcwd()
+    file_ = os.path.join(dir_, "master_pipeline.py")
+    result = subprocess.run([file_, "dir", "-c", "-s"])
+    assert result.returncode == 0
