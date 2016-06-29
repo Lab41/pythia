a2bf0cb08108814d2a096f5d53c26024e42638a3
Audrey Flower <aflower@dev-lt-4257.local>
2016-06-29 15:54:28 -0700

    improved setup

--- src/tests/test_master_pipeline.py
+++ src/tests/test_master_pipeline.py
@@ -10,7 +10,7 @@ import os
 def setup_module(module):
     os.chdir('src/pipelines')
 
-def test_no_directory(set_directory):
+def test_no_directory():
     dir_ = os.getcwd()
     file_ = os.path.join(dir_, "master_pipeline.py")
     result = subprocess.run([file_])
