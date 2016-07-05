cdf05848955f803f3a744ed3094e8c3076f4700d
pcallier <pcallier@iqt.org>
2016-07-05 23:43:55 +0000

    Fix typo in test

--- src/tests/test_tokenize.py
+++ src/tests/test_tokenize.py
@@ -5,7 +5,7 @@ from src.utils import tokenize
 def test_punkt():
     """Test sentence tokenization"""
 
-    assert tokenize.punkt_sentences("S1. S2. S3! S4!!!") == ["S1.", "S2.", "S3!", "S4!!, "!"]
+    assert tokenize.punkt_sentences("S1. S2. S3! S4!!!") == ["S1.", "S2.", "S3!", "S4!!", "!"]
     assert tokenize.punkt_sentences("S1.      S4!!!") == ["S1.", "S4!!, "!"]
 
 def test_word_punct():
