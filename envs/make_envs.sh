1a61de5809844320f29a19d31719706707b2537b
patrickc <pcallier@iqt.org>
2016-07-08 13:05:15 -0700

    Error checking

--- envs/make_envs.sh
+++ envs/make_envs.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 # Create conda environments and Jupyter kernels for Pythia
-# Adds Pythia repo root to PYTHONPATH so its source tree can be imported 
+# Adds Pythia repo root to PYTHONPATH so its source tree can be imported
 # in Python
 # Requires Anaconda and Jupyter
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+set -e
+
 PYTHIA_CONFIG="$1"
 if [ "$PYTHIA_CONFIG" = "" ]; then
-    printf "Must pass in JSON object of configuration variables.\nSuggested usage:"
-    printf "make_envs.sh <(echo \"{\\\\\"PYTHONPATH\\\\\":\\\\\"\$PYTHONPATH\\\\\"}\")\n"
-    exit 1
+    printf "Did not pass in JSON file of configuration variables.\n"
+    printf "Continuing with PYTHONPATH pass-through...\n"
+    printf "Alternative usage: \n"
+    printf "\tmake_envs.sh config.json\n\n"
+    PYTHIA_CONFIG='{ "PYTHONPATH" : "'"$PYTHONPATH"'" }'
 else
     PYTHIA_CONFIG="$(cat $PYTHIA_CONFIG)"
 fi
@@ -18,16 +24,19 @@ make_env () {
     env_name="$1"
     display_name="$2"
     python_version="$3"
-    
+
     echo $env_name
     echo $display_name
     echo $python_version
     echo $PYTHIA_CONFIG
 
 
+set +e
+    # Does not work with BSD grep (OS X)
     search_for_environment="$(conda info -e 2>/dev/null | grep -Po '^ *'$env_name'(?= )' | head -n1)"
     echo "Matched environment line: $search_for_environment"
-    source deactivate 2>/dev/null
+    source deactivate 2>/dev/null || true
+set -e
     sleep 2
     if [ "$search_for_environment" = "$env_name" ]; then
         echo "Environment exists, installing original configuration..."
@@ -75,7 +84,7 @@ make_env () {
         <(echo ", ") \
         <(sed '1d' "$kernel_path" ) > /tmp/kernel.json
     mv /tmp/kernel.json "$kernel_path"
-    
+
     cat "$kernel_path" && echo ""
 
 }
