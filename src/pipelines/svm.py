e5bef24db6298b1c8c87cc60c1c985107fe44736
bethke <anna.bethke@gmail.com>
2016-07-20 10:35:04 -0700

    Expose all parameters, give ability to pass in these parameters as a dictionary with args and kwargs

--- src/pipelines/svm.py
+++ src/pipelines/svm.py
@@ -7,7 +7,9 @@ from sklearn import svm
 from sklearn.svm import SVC
 from collections import namedtuple
 
-def run_model(train_data, train_labels, test_data, test_labels):
+def run_model(train_data, train_labels, test_data, test_labels, svm_C=1.0, svm_kernel='rbf', svm_degree=3, svm_gamma='auto', svm_coef0=0.0,
+              svm_shrinking=True, svm_probability=False, svm_tol=0.001, svm_cache_size=200, svm_class_weight=None, svm_verbose=False,
+              svm_max_iter=-1, svm_decision_function_shape=None, svm_random_state=None, *args, **kwargs):
     '''
     Algorithm which will take in a set of training text and labels to train a bag of words model
     This model is then used with a logistic regression algorithm to predict the labels for a second set of text
@@ -22,7 +24,9 @@ def run_model(train_data, train_labels, test_data, test_labels):
     '''
 
     #use Logistic Regression to train a model
-    svm = SVC()
+    svm = SVC(C=svm_C, kernel=svm_kernel, degree=svm_degree, gamma=svm_gamma, coef0=svm_coef0, shrinking=svm_shrinking,
+              probability=svm_probability, tol=svm_tol, cache_size=svm_cache_size, class_weight=svm_class_weight,
+              verbose=svm_verbose, max_iter=svm_max_iter, decision_function_shape=svm_decision_function_shape, random_state=svm_random_state)
 
     # we create an instance of Neighbours Classifier and fit the data.
     svm.fit(train_data, train_labels)
@@ -41,9 +45,14 @@ def main(argv):
 
     train_data, train_target, test_data, test_target = argv[0], argv[1], argv[2], argv[3]
 
+    if len(argv)>4:
+        args_dict = argv[4]
+    else:
+        args_dict = {}
+
     print("running support vector machine...",file=sys.stderr)
 
-    predicted_labels, perform_results = run_model(train_data, train_target, test_data, test_target)
+    predicted_labels, perform_results = run_model(train_data, train_target, test_data, test_target, **args_dict)
 
     return predicted_labels, perform_results
 
