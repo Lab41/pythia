27329ae742990efc0808d1f0e8563f909a1a12a2
Chris Nowlin <cnowlin@gmail.com>
2016-08-11 15:16:00 -0700

    Mistakes were made. May have fixed them.

--- src/featurizers/skipthoughts/decoding/vocab_broken.py
+++ src/featurizers/skipthoughts/decoding/vocab_broken.py
@@ -1,9 +1,9 @@
 """
 Constructing and loading dictionaries
 """
+import cPickle as pkl
 import numpy
 from collections import OrderedDict
-import pickle
 
 def build_dictionary(text):
     """
@@ -14,20 +14,17 @@ def build_dictionary(text):
     for cc in text:
         words = cc.split()
         for w in words:
-            wordcount[w] = wordcount.get(w,0) + 1
-
-    ####
-    words = list(wordcount.keys())
-    freqs = list(wordcount.values())
+            if w not in wordcount:
+                wordcount[w] = 0
+            wordcount[w] += 1
+    words = wordcount.keys()
+    freqs = wordcount.values()
     sorted_idx = numpy.argsort(freqs)[::-1]
 
     worddict = OrderedDict()
     for idx, sidx in enumerate(sorted_idx):
         worddict[words[sidx]] = idx+2 # 0: <eos>, 1: <unk>
-    ###
-        
-    #Alternative: OrderedDict((word,idx + 2) for idx,(word,count) in enumerate(sorted(wordcount.items(),key=lambda _:_[1],reverse=True)))
-    
+
     return worddict, wordcount
 
 def load_dictionary(loc='/ais/gobi3/u/rkiros/bookgen/book_dictionary_large.pkl'):
@@ -35,7 +32,7 @@ def load_dictionary(loc='/ais/gobi3/u/rkiros/bookgen/book_dictionary_large.pkl')
     Load a dictionary
     """
     with open(loc, 'rb') as f:
-        worddict = pickle.load(f)
+        worddict = pkl.load(f)
     return worddict
 
 def save_dictionary(worddict, wordcount, loc):
@@ -43,7 +40,7 @@ def save_dictionary(worddict, wordcount, loc):
     Save a dictionary to the specified location 
     """
     with open(loc, 'wb') as f:
-        pickle.dump(worddict, f)
-        pickle.dump(wordcount, f)
+        pkl.dump(worddict, f)
+        pkl.dump(wordcount, f)
 
 
