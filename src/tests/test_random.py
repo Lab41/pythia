6351c2e61a7811be75cab3a31b8cc85a856b276f
pcallier <pcallier@iqt.org>
2016-08-23 23:10:22 +0000

    Add tests for deterministic pseudo-random behavior

--- src/tests/test_random.py
+++ src/tests/test_random.py
@@ -1,5 +1,5 @@
 import py.test
-from src.pipelines import get_args, main as pipeline_main
+from src.pipelines.master_pipeline import get_args, main as pipeline_main
 
 
 #FIXME: Should make so many of these for different arguments
@@ -16,6 +16,31 @@ def test_random():
     assert run1==run2
                     
         
+def test_random_logreg():
+    args = get_args(SEED=41, 
+                    directory='data/stackexchange/anime', 
+                    XGB=False,
+                    LOG_REG=True,
+                    LDA_APPEND=False,
+                    BOW_APPEND=True,
+                    W2V_APPEND=False,
+                    RESAMPLING=False)
+    run1 = pipeline_main(args)
+    run2 = pipeline_main(args)
+    assert run1==run2
+
+def test_random_svm():
+    args = get_args(SEED=41, 
+                    directory='data/stackexchange/anime', 
+                    XGB=False,
+                    SVM=True,
+                    LDA_APPEND=False,
+                    BOW_APPEND=True,
+                    W2V_APPEND=False,
+                    RESAMPLING=False)
+    run1 = pipeline_main(args)
+    run2 = pipeline_main(args)
+
 def test_random_resampling():
     args = get_args(SEED=41, 
                     directory='data/stackexchange/anime', 
